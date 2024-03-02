ffa13bd89ed333d309defe365d9434d6bedda313
盧奕樺 <shepherdvilu@gmail.com>
2024-03-02 16:10:50 +0800

    Update lesson_4.3.sql

--- SQL/lesson_4.3.sql
+++ SQL/lesson_4.3.sql
@@ -47,3 +47,44 @@ SELECT *
 FROM student
 ORDER BY score 
 LIMIT 2;
+
+SELECT *
+FROM student
+WHERE major = '英語'
+ORDER BY score DESC
+LIMIT 1;
+
+
+
+
+
+SELECT *
+FROM student
+WHERE major = '英語' OR major = '生物' OR major = '歷史';
+
+/*也可以用IN表示*/
+SELECT *
+FROM student
+WHERE major IN('英語','生物','歷史');
+
+SELECT *
+FROM student
+WHERE score >= 80 AND score <= 100;
+
+/*也可以用BETWEEN AND表示*/
+SELECT *
+FROM student
+WHERE score BETWEEN 80 AND 100;
+
+SELECT *
+FROM student
+WHERE NOT name='小黃';
+
+/*第一個字為小的*/
+SELECT *
+FROM student
+WHERE name LIKE '小%';
+
+SELECT *
+FROM student
+WHERE name LIKE '%黃%';
