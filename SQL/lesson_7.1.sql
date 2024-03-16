0f92ccf565fa8d7e69f12d250446d56a494595ee
盧奕樺 <shepherdvilu@gmail.com>
2024-03-16 10:01:16 +0800

    Update lesson_7.1.sql

--- SQL/lesson_7.1.sql
+++ SQL/lesson_7.1.sql
@@ -1,15 +1,17 @@
 /*進站人數最多的一筆*/
 
-select * from gate_count left join stations on 站點編號= 編號
-where 進站人數 =82586
+SELECT *
+FROM gate_count LEFT JOIN stations ON 站點編號 = 編號
+WHERE 進站人數 = 82586
 
+SELECT MAX(進站人數)
+FROM gate_count
 
-select max(進站人數) from gate_count
-
-
-select * from gate_count left join stations on 站點編號= 編號
-where 進站人數 in(select max(進站人數) from gate_count)
-
-
+SELECT *
+FROM gate_count LEFT JOIN stations ON 站點編號 = 編號
+WHERE 進站人數 = (
+	SELECT MAX(進站人數)
+	FROM gate_count
+);
 
 
